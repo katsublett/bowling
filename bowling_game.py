09d9dd6a66ef2f184ef41856e3c89fcaaa2c581d
Kat Sublett <38798134+katsublett@users.noreply.github.com>
2021-05-01 01:37:25 -0400

    refactor: create utility methods for bonuses/score The bonus and frame scores are creating bloated assignment of values and are not clear in the purpose of the calculations. Break up the the calcualtions into their own utility functions.

--- bowling_game.py
+++ bowling_game.py
@@ -13,22 +13,28 @@ class BowlingGame:
     def is_strike(self, first_ball_in_frame):
         return self.rolls[first_ball_in_frame] == 10
 
+    def spare_bonus(self, first_ball_in_frame):
+        return self.rolls[first_ball_in_frame + 2]
+
+    def strike_bonus(self, first_ball_in_frame):
+        return self.rolls[first_ball_in_frame + 1] + self.rolls[first_ball_in_frame + 2]
+
+    def frame_score(self, first_ball_in_frame):
+        return self.rolls[first_ball_in_frame] + self.rolls[first_ball_in_frame + 1]
+
     def get_score(self):
         score = 0
         first_ball_in_frame = 0
 
         for frame in range(0, 10):
             if self.is_strike(first_ball_in_frame):
-                score += 10 + \
-                    self.rolls[first_ball_in_frame + 1] + \
-                    self.rolls[first_ball_in_frame + 2]
+                score += 10 + self.strike_bonus(first_ball_in_frame)
                 first_ball_in_frame += 1
             if self.is_spare(first_ball_in_frame):
-                score += 10 + self.rolls[first_ball_in_frame + 2]
+                score += 10 + self.spare_bonus(first_ball_in_frame)
                 first_ball_in_frame += 2
             else:
-                score += self.rolls[first_ball_in_frame] + \
-                    self.rolls[first_ball_in_frame + 1]
+                score += self.frame_score(first_ball_in_frame)
                 first_ball_in_frame += 2
 
         return score
