9bd835efd8951658dd1cdc2c85c130e5c62a1ed7
Kat Sublett <38798134+katsublett@users.noreply.github.com>
2021-05-01 01:45:42 -0400

    refactor: rename variables to be more explanatory The variable 'rolls' is not clear enough in its purpose. Rename to frame as that is more aligned with how the variable is utilized. Variables that are too long can detract from its usefulness. Rename the variable 'first_ball_in_frame' to 'first_ball' as the renaming of 'rolls' to 'frame'  will provide further context that the ball is for that frame.

--- bowling_game.py
+++ bowling_game.py
@@ -1,40 +1,40 @@
 class BowlingGame:
     def __init__(self):
-        self.rolls = [0] * 21
+        self.frame = [0] * 21
         self.current_roll = 0
 
     def roll_ball(self, pins):
-        self.rolls[self.current_roll] = pins
+        self.frame[self.current_roll] = pins
         self.current_roll += 1
 
-    def is_spare(self, first_ball_in_frame):
-        return self.rolls[first_ball_in_frame] + self.rolls[first_ball_in_frame + 1] == 10
+    def is_spare(self, first_ball):
+        return self.frame[first_ball] + self.frame[first_ball + 1] == 10
 
-    def is_strike(self, first_ball_in_frame):
-        return self.rolls[first_ball_in_frame] == 10
+    def is_strike(self, first_ball):
+        return self.frame[first_ball] == 10
 
-    def spare_bonus(self, first_ball_in_frame):
-        return self.rolls[first_ball_in_frame + 2]
+    def spare_bonus(self, first_ball):
+        return self.frame[first_ball + 2]
 
-    def strike_bonus(self, first_ball_in_frame):
-        return self.rolls[first_ball_in_frame + 1] + self.rolls[first_ball_in_frame + 2]
+    def strike_bonus(self, first_ball):
+        return self.frame[first_ball + 1] + self.frame[first_ball + 2]
 
-    def frame_score(self, first_ball_in_frame):
-        return self.rolls[first_ball_in_frame] + self.rolls[first_ball_in_frame + 1]
+    def frame_score(self, first_ball):
+        return self.frame[first_ball] + self.frame[first_ball + 1]
 
     def get_score(self):
         score = 0
-        first_ball_in_frame = 0
+        first_ball = 0
 
         for frame in range(0, 10):
-            if self.is_strike(first_ball_in_frame):
-                score += 10 + self.strike_bonus(first_ball_in_frame)
-                first_ball_in_frame += 1
-            if self.is_spare(first_ball_in_frame):
-                score += 10 + self.spare_bonus(first_ball_in_frame)
-                first_ball_in_frame += 2
+            if self.is_strike(first_ball):
+                score += 10 + self.strike_bonus(first_ball)
+                first_ball += 1
+            if self.is_spare(first_ball):
+                score += 10 + self.spare_bonus(first_ball)
+                first_ball += 2
             else:
-                score += self.frame_score(first_ball_in_frame)
-                first_ball_in_frame += 2
+                score += self.frame_score(first_ball)
+                first_ball += 2
 
         return score
