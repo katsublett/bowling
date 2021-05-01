3ecb762ea9fad685abc6303c07d46d5de6306b6b
Kat Sublett <38798134+katsublett@users.noreply.github.com>
2021-05-01 00:43:00 -0400

    test: create utility method for spare rolls Refactor the test for rolling 1 spare by creating a utility function for specifically rolling spares. The refactoring allows for the utility to be used again in future tests if necessary.

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -12,6 +12,10 @@ class TestBowlingGame(unittest.TestCase):
         for x in range(frame):
             self.game.roll_ball(pins)
 
+    def roll_spare(self):
+        self.game.roll_ball(5)
+        self.game.roll_ball(5)
+
     def test_gutter_game(self):
         self.roll_many(20, 0)
         self.assertEqual(0, self.game.get_score())
@@ -19,6 +23,12 @@ class TestBowlingGame(unittest.TestCase):
     def test_all_ones_game(self):
         self.roll_many(20, 1)
         self.assertEqual(20, self.game.get_score())
+
+    def test_one_spare_game(self):
+        self.roll_spare()
+        self.game.roll_ball(7)  # bonus ball (counted twicce)
+        self.roll_many(17, 0)
+        self.assertEqual(24, self.game.get_score())
 
 
 if __name__ == '__main__':
