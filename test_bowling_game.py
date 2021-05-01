5f51740bfd08d593e9114f174d2ad7d85b407dfa
Kat Sublett <38798134+katsublett@users.noreply.github.com>
2021-04-30 22:33:24 -0400

    test: create test for rolling a ball Testing the method used for setting the number of pins knocked down when a bowling ball is rolled in a game.

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -8,6 +8,10 @@ class TestBowlingGame(unittest.TestCase):
     def test_create_game(self):
         game = BowlingGame()
 
+    def test_roll_ball(self):
+        game = BowlingGame()
+        game.roll_ball(5)
+
 
 if __name__ == '__main__':
     unittest.main()
