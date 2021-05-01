aef300dc8c390cae1f1c24b9b0eb90ffe27003c7
Kat Sublett <38798134+katsublett@users.noreply.github.com>
2021-05-01 00:55:13 -0400

    refactor: add `is_spare` to check if turn is spare The is_spare method was created in order to break out the calculation of the rolls in a turn and checking whether the outcome was a spare or not. The breaking up of this calculation into its own method allows for the reusability of the functionality in other methods in the future. We can also ensure that we are maintaining the DRY principle.

--- bowling_game.py
+++ bowling_game.py
@@ -7,12 +7,15 @@ class BowlingGame:
         self.rolls[self.current_roll] = pins
         self.current_roll += 1
 
+    def is_spare(self, first_ball_in_frame):
+        return self.rolls[first_ball_in_frame] + self.rolls[first_ball_in_frame + 1] == 10
+
     def get_score(self):
         score = 0
         first_ball_in_frame = 0
 
         for frame in range(0, 10):
-            if self.rolls[first_ball_in_frame] + self.rolls[first_ball_in_frame + 1] == 10:
+            if self.is_spare(first_ball_in_frame):
                 score += 10 + self.rolls[first_ball_in_frame + 2]
                 first_ball_in_frame += 2
             else:
