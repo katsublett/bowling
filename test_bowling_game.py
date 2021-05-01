8de5328ca3dabe232654d84dc54272bcec8dd865
Kat Sublett <38798134+katsublett@users.noreply.github.com>
2021-04-30 23:20:42 -0400

    test: create test for knocking down 1 pin per roll

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -13,6 +13,11 @@ class TestBowlingGame(unittest.TestCase):
             self.game.roll_ball(0)
         self.assertEqual(0, self.game.get_score())
 
+    def test_all_ones_game(self):
+        for pins in range(20):
+            self.game.roll_ball(1)
+        self.assertEqual(20, self.game.get_score())
+
 
 if __name__ == '__main__':
     unittest.main()
